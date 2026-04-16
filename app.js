8685abe739df9842ffdd42be236d4f74cbbfc2d9
mindedcode <adarshkumar.golu8181@gmail.com>
2026-04-17 02:09:51 +0530

    Finally Done

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ $(document).ready(function() {
 let hamberger = document.querySelector('.hamberger');
 let times = document.querySelector('.times');
 let mobileNav = document.querySelector('.mobile-nav');
+let about = document.querySelector('.about');
+let home = document.querySelector('a[href="#home"]');
+let skills = document.querySelector('a[href="#skills"]');
+let certificates = document.querySelector('a[href="#certificates"]');
+let projects = document.querySelector('a[href="#projects"]');
+let contact = document.querySelector('a[href="#contact"]');
 
 hamberger.addEventListener('click', function () {
     mobileNav.classList.add('open');
@@ -18,5 +24,40 @@ times.addEventListener('click', function(){
     mobileNav.classList.remove('open');
 });
 mobileNav.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
 
+about.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+})
+hamberger.addEventListener('click', function () {
+    mobileNav.classList.add('open');
+});
+
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+about.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+home.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+skills.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+certificates.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+projects.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+contact.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
 });
